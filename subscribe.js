a1bf9981c4914e3a5f493780ee8eba9b929df7cf
tyrone17 <tyronemaasdorp@gmail.com>
2023-09-11 16:41:54 +0200

    ou
    added xml to test Xhttp

--- subscribe.js
+++ subscribe.js
@@ -1,54 +1,99 @@
 // create IDs
 acpusubID = Date.now();
-var localEID = localStorage.setItem("subID", acpuserID);
-var localRID = localStorage.getItem("subID");
-var currentUser =[
-    acpuserID=localRID,
+localStorage.setItem("subID", acpusubID);
+var oSubID = localStorage.getItem("subID");
+console.log(typeof oSubID);
 
-];
-// Verify email first in from
+function add(){
+  getAtt();
+  person = [
+    user=who,
+    wallet=coins,
+    tx=transactions
+  ];
+  
+  localStorage.setItem("person",person);
+  
+}
+
+function getAtt(){
+  // Fetch form data vars to use
+  
+  // Fetch user consent check
+  var c = document.getElementById("tac").textContent;
+  var consent = c.charAt(6)
+  var e = document.getElementById("email").value;
+  var n = document.getElementById("number").value;
+  subscriberInfoDB = [ 
+    currentUserEmail= e,
+    currentUserNum= n,
+    consentGiven= consent,
+    oSubID= oSubID
+  ];
+  console.log(subscriberInfoDB)
+  localStorage.setItem("subscriberInfoDB",subscriberInfoDB)
+ 
+  return ;
+}
+
+document.getElementById("subLink").style.display = "hidden";
+
+// TODO fetch from other pages script
+var user = localStorage.getItem("userID");
+
+
+// TODO Verify email in from
+
+
 // 	TODO Allocation user EXP for bonus system
-var electrons =0.0001; 
+var ecoins =0.0001; 
+
 // START app
-console.log("acp-blog webapp: v1.0.1 - "+ localRID);
-var siteSubID = [
-    id= localRID,
-    electrons = electrons
-];
-console.log("acpsub no:. : " + siteSubID.id);
+console.log("acp-blog webapp: v1.0.1");
 
-function runDMC (siteSubIDid,siteSubIDelectrons){
+// TODO create an dic
+var who = oSubID;
+var coins = ecoins;
+var transactions =[];
+
+function createSubscriber (user,coins){
     // Exec the code to create incesion into app.
-    siteSubID.id= localRID;
-    siteSubIDId=siteSubID.id;
-    siteSubID.electrons= electrons;
-    siteSubIDelectrons= siteSubID.electrons;
-    console.log("acpsub electrons wallet balance:. : " + siteSubIDelectrons);
+	subscriberDB = [ 
+        currentUser= person.user,
+        loggedInUser= person.user,
+    ];
+	
+	localStorage.setItem("subscriberDB",subscriberDB);
+  
 }
 
-runDMC();
+//if (localStorage.getItem("subscriberDB") != null && document.){
+//	return;
+//} else{
+//	createSubscriber();
+//};
 
 
-function fetchDMC (){
+//function fetchDMC (){
     // exec the code to create a suture
-    var userPro = localStorage.getItem("subID");
-    var userRootApp = localStorage.getItem("userID");
-    console.log(userPro);
-    checkTwo = true;
+  //  var userPro = localStorage.getItem("subID");
+    //var userRootApp = localStorage.getItem("userID");
+    //console.log(userPro);
+    //checkTwo = true;
     
-    userDB=[ 
-        currentUser=userPro,
-        loginInUser=userRootApp,
-        checkTwo = true,
-        coins=siteSubID.electrons
+   // userDB=[ 
+    //    currentUser=userPro,
+     //   loginInUser=userRootApp,
+       // checkTwo = true,
+       // coins=siteSubID.electrons
         
-    ]
-    localStorage.setItem("userDB", userDB) 
-    console.log("current MAC Address obtained: " + coins + " sigmasixsofa\n")
-    console.log(siteSubID.electrons)
-}
+    //]
+    //localStorage.setItem("userDB", userDB) 
+    //console.log("current MAC Address obtained: " + coins + " sigmasixsofa\n")
+    //console.log(siteSubID.electrons)
+//}
 
-fetchDMC()
+//fetchDMC()
 
 // export default function DAAP (){
 //     var ce = fetchDMC();
