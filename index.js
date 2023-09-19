d0c8dfcf598a15f542bb6f5201ed9c0c81363f87
tyrone17 <tyronemaasdorp@gmail.com>
2023-09-19 13:13:30 +0200

    Updated html
    And replaced content with links to YT

--- index.js
+++ index.js
@@ -11,15 +11,13 @@ console.log("'success factors in mistakes'- Tyrone Maasdorp");
 acpuserID = Date.now();
 var tUserID = localStorage.getItem("userID");
 console.log("acpuser: " + acpuserID);
-var jvar = localStorage.getItem("userDB");
-// locales
-console.log("DB info: " + jvar);
+
 // local wallet
 var eAllocation= 0.0001;
 //console.log(tSubID, eAllocation)
 sPerson = {ACPuserID:tUserID, eAllocation:eAllocation, number:"", email:""};
 // localStorage.setItem("person",sPerson)
 // ID and Wallet info
-console.log("Number on file: "+sPerson.number, " current balance of e-token: " + eAllocation)
+console.log("Number on file: "+sPerson.number, " Email on file: "+sPerson.email, " current balance of e-token: " + eAllocation)
 
 
